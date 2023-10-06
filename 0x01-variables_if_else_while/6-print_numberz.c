e31b8dd22fc8bd9c911a859f7c40c6a2fe0e6fae
tomiwa <codertomiwa@gmail.com>
2023-10-06 15:37:04 -0700

    6-print_numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: 0 when successful
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i <= 10 ; i++)
+		putchar(i);
+	putchar(i);
+	return (0);
+}
