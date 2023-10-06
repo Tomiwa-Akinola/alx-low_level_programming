8d9265daccc7171433e20f37a80f7217d9ded942
tomiwa <codertomiwa@gmail.com>
2023-10-06 00:32:51 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - this is the entry point of the program where the execution starts
+ * Return: always 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
