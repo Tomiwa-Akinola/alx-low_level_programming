5dc75b22eb084ab02f8c02fa1e1675db4ac0f0ba
tomiwa <codertomiwa@gmail.com>
2023-10-06 14:06:50 -0700

    4-print_alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - entry point of program
+ * Return: 0 when successful
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+		if (i != 'q' && i != 'e')
+			putchar(i);
+	putchar('\n');
+	return (0);
+}
