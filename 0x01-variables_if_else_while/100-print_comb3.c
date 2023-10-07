19a2bb71f659869a496f2d8359283276b6233099
tomiwa <codertomiwa@gmail.com>
2023-10-06 23:28:28 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: 0 when succesful
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 0 ; i <= 9; i++)
+	{
+		for (j = 0 ; j <= 9; j++)
+		{
+			if (i != j && i - j != 1 && j > i)
+			{
+				putchar(i + '0');
+				putchar(j + '0');
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	return (0);
+}
