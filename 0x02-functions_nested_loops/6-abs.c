6fade86f523e9ddb9e7f3ef435a7b164648d1f18
tomiwa <codertomiwa@gmail.com>
2023-10-10 20:15:02 -0700

    6-abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - it computes absolute integer
+ * @i: parameter
+ * Return: postive number
+ */
+
+
+int _abs(int i)
+{
+	if (i < 0)
+		return (-i);
+	else
+		return (i);
+}
