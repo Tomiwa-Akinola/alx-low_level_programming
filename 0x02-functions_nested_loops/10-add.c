f314b7d2d53f59934b5246f9ed37a5918cdea9ce
tomiwa <codertomiwa@gmail.com>
2023-10-12 11:26:08 -0700

    10-add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * add - addition function
+ * @int i: parameter for function add
+ * @int j: parameter for function add
+ * Return: k
+ */
+
+int add(int i, int j)
+{
+	int k;
+
+	k = i + j;
+	return (k);
+}
