f5d33832b50803f84f213d2a61a2b8fe0c613469
tomiwa <codertomiwa@gmail.com>
2023-10-12 18:30:18 -0700

    print 0 to 14

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * more_numbers - print 0 to 14 ten times
+ * Return: void
+ */
+
+void more_numbers(void)
+{
+	int a, b;
+
+	for (b = 0 ; b <= 10 ; b++)
+	{
+		for (a = 0 ; a <= 14 ; a++)
+		{
+			if (a > 9)
+				_putchar(a / 10 + '0');
+			_putchar(a % 10 + '0');
+		}
+		_putchar('\n');
+	}
+}
