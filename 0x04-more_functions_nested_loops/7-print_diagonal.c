89d27b5e61ccf70cf8fad2b1a8ebaa150a74791c
tomiwa <codertomiwa@gmail.com>
2023-10-13 08:10:19 -0700

    8-print_square.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -14,12 +14,20 @@ void print_diagonal(int n)
 	{
 		int i, j;
 
-		for (i = 1 ; i <= n ; i++)
+		for (i = 0 ; i <= n ; i++)
 		{
 			for (j = 0 ; j <= i ; j++)
-				_putchar(' ');
-			_putchar('\\');
-			_putchar('\n');
+			{
+				if (i == 0)
+				{
+					_putchar('\\');
+					_putchar('\n');
+				}
+				else
+					_putchar(' ');
+				_putchar('\\');
+				_putchar('\n');
+			}
 		}
 	}
 }
