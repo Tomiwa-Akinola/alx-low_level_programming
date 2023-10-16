9992d61641d40f1cf156e253c9e107d3c839cb24
tomiwa <codertomiwa@gmail.com>
2023-10-16 14:11:18 -0700

    reverse string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ * @s: parameter
+ * Return: void
+ */
+
+void rev_string(char *s)
+{
+	int i;
+	int j = 0;
+
+	for (i = 0 ; s[i] != '\0' ; i++)
+		j++;
+
+	for (i = 0 ; i < j / 2 ; i++)
+	{
+		char g;
+
+		g = s[i];
+		s[i] = s[j - 1 - i];
+		s[j - 1 - i] = g;	
+	}
+}
