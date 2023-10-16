93c74a1e2592c18bc43c859943de92f22e600faf
tomiwa <codertomiwa@gmail.com>
2023-10-16 11:09:53 -0700

    prints in reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -2,7 +2,7 @@
 
 /**
  * print_rev - prints a string in reverse
- * @s - parameter
+ * @s: parameter
  * Return: void
  */
 
@@ -14,7 +14,7 @@ void print_rev(char *s)
 	for (i = 0 ; s[i] != '\0' ; i++)
 		j++;
 
-	for (i = j -1 ; s[i] != 0 ; i--)
+	for (i = j - 1 ; s[i] != 0 ; i--)
 		_putchar(s[i]);
 	_putchar('\n');
 }
