a648279b729a6522422b9f2cc6a50f8da3a4b699
tomiwa <codertomiwa@gmail.com>
2023-10-17 08:07:03 -0700

    sum of multiples

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -8,11 +8,14 @@
 int main(void)
 {
 	int i;
+	int j = 0;
 
 	for (i = 0 ; i < 1024 ; i++)
 	{
-		if (i % 3 == 0)
-			printf("%d, ", i);
+		if (i % 3 == 0 || i % 5 == 0)
+			j = j + i;	
 	}
+	printf("%d", j);
+	printf("\n");
 	return (0);
 }
