da8d9c9cb3f985a2a07dbda6c5aec605906abef7
tomiwa <codertomiwa@gmail.com>
2023-10-17 10:13:22 -0700

    104-fibonacci.c

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - prints Fibonacci numbers starting with 1 and 2
+ * Return: 0
+ */
+
+int main(void)
+{
+	unsigned long int a = 1;
+	unsigned long int b = 2;
+	unsigned long int next;
+	int i;
+
+	printf("%lu, ", a);
+	for (i = 1 ; i < 98 ; i++)
+	{
+		printf("%lu", b);
+		if (i != 97)
+			printf(", ");
+		next = a + b;
+		a = b;
+		b = next;
+	}
+	printf("\n");
+	return (0);
+}
