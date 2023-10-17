4600d6073156fccb014203ed2f525d2d190b2e47
tomiwa <codertomiwa@gmail.com>
2023-10-16 22:05:30 -0700

    crackme

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _atoi - converts a string to an integer
+ * @s: parameter
+ * Return: 0 if there are no numbers
+ */
+
+int _atoi(char *s)
+{
+	int i = 0;
+	int j = 1;
+	unsigned int k = 0;
+	int l = 0;
+
+	while (s[c])
+	{
+		if (s[c] == 45)
+		{
+			j *= -1;
+		}
+	}
+
+}
