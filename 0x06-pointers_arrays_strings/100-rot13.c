66e705098ad27b21c13d19228a32988f3f57cf62
tomiwa <codertomiwa@gmail.com>
2023-10-20 11:04:52 -0700

    rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -15,10 +15,13 @@ char *rot13(char *s)
 
 	for (i = 0 ; s[i] != '\0' ; i++)
 	{
-		for (j = 0 ; j < 52 ; j++)
+		for (j = 0 ; a[j] != '\0' ; j++)
 		{
 			if (s[i] == a[j])
+			{
 				s[i] = b[j];
+				break;
+			}
 		}
 	}
 	return (s);
