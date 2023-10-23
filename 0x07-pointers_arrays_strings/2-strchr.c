2bd01650a1c1c8f01a3c60784ae5023fce8a84eb
tomiwa <codertomiwa@gmail.com>
2023-10-23 10:19:03 -0700

    2-strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strchr - locates a character in a string
+ * @s: Parameter 1
+ * @c: Parameter 2
+ * Return: 0 or s
+ */
+
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+			return (s);
+		else if (*s == c)
+			return(0);
+		s++;
+	}
+	return (0);
+}
