8d5b0391d9cf7bd9f1605aff9e5aaaa3c02302fb
tomiwa <codertomiwa@gmail.com>
2023-10-23 20:20:57 -0700

    locate substring

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -9,5 +9,18 @@
 
 char *_strstr(char *haystack, char *needle)
 {
-	
+	for (; *haystack != 0 ; haystack++)
+	{
+		char *one = haystack;
+		char *two = needle;
+
+		while (*one == *two && *two != '\0')
+		{
+			one++;
+			two++;
+		}
+		if (*two == '\0')
+			return (haystack);
+	}
+	return (NULL);
 }
