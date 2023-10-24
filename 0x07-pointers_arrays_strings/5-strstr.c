01100eea447af0267ee33c00d12abf8817d8caab
tomiwa <codertomiwa@gmail.com>
2023-10-23 19:08:07 -0700

    7-print_chessboard

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _strstr - locates a substring
+ * @haystack: parameter 1
+ * @needle: parameter 2
+ * Return: char or NULL
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	
+}
