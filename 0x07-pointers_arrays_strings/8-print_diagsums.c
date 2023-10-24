a6cc8a241599b6e337cd0a8ec4ffa0a2bc6474bf
tomiwa <codertomiwa@gmail.com>
2023-10-23 21:06:46 -0700

    8-print_diagsums

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagsums - prints the sum of two diagonals of a sqyare
+ * matrix of intergers
+ * @a: parameter 1
+ * @b: parameter 2
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int i, n;
+	int sum1 = 0;
+	int sum2 = 0;
+
+	for (i = 0 ; i <= (size * size) ; i = i + size + 1)
+		sum1 = sum1 + a[i];
+
+	for (n = size - 1 ; n <= (size * size) - size ; n = n + size - 1)
+		sum2 = sum2 + a[n];
+	printf("%d, %d\n", sum1, sum2);
+}
