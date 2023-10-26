9e1d37c45ef4e2a49e69eb7b99881435c101713c
tomiwa <codertomiwa@gmail.com>
2023-10-26 09:43:11 -0700

    100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -23,7 +23,7 @@ int _strlen_recursion(char *s)
 
 int compare_string(char *s, int left, int right)
 {
-	if (*s(s * left) == *(s + right))
+	if (*(s + left) == *(s + right))
 	{
 		if (left == right || left == right + 1)
 			return (1);
@@ -42,5 +42,5 @@ int is_palindrome(char *s)
 {
 	if (*s == '\0')
 		return (1);
-	return (compare_string(s, 0, strlen_recursion(s - 1)));
+	return (compare_string(s, 0, _strlen_recursion(s) - 1));
 }
