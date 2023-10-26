d4525f1b3ea02dc1bd8054b353412581ccd6331c
tomiwa <codertomiwa@gmail.com>
2023-10-26 10:01:04 -0700

    5-sqrt

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -2,21 +2,18 @@
 
 /**
  * sqrt_a - returns natural square root
- * @n: parameter
+ * @a: parameter
  * @c: iterator
  * Return: square root or -1
  */
 
 int sqrt_a(int a, int b)
 {
-	int sqrt_a = b * b;
-
-	if (sqrt_a > a)
-		return (-1);
-	else if (sqrt_a == a)
+	if (b * b == a)
 		return (b);
-	else
-		return (sqrt_a(a, b + 1));
+	else if (b * b > a)
+		return (-1);
+	return (sqrt_a(a, b + 1));
 }
 /**
  * _sqrt_recursion - returns square root of a number
