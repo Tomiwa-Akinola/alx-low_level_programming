52cb9b1e55ca85c73155100e09eb671558a7bd6d
tomiwa <codertomiwa@gmail.com>
2023-10-27 05:53:41 -0700

    libmy

---
+++ 0x09-static_libraries/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isdigit - entry point of program
+ * @c: parameter
+ * Return: 0 or 1
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+		return (1);
+	else
+		return (0);
+}
