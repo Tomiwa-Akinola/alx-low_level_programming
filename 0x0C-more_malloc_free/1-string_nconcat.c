6cb9ddbcb138c1528b061c78600c16f6e1689183
tomiwa <codertomiwa@gmail.com>
2023-11-02 08:52:20 -0700

    1-string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -42,7 +42,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 			output[i] = s1[i];
 		for (i = 0 ; i < n ; i++)
 			output[len1 + i] = s2[i];
-		output[len1 + i] = '\0';			
+		output[len1 + i] = '\0';
 	}
 	return (output);
 }
