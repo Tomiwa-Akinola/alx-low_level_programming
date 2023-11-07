72f32a6d3d7ec363cb3d4fc8a2f8f8acb472addb
tomiwa <codertomiwa@gmail.com>
2023-11-06 16:19:26 -0800

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog descrioption
+ * @name: of dog
+ * @age: of dog
+ * @owner: of dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
