d7a961dca9f32b855f91a2407a05fb3fb7981b49
tomiwa <codertomiwa@gmail.com>
2024-01-15 10:23:16 -0800

    libhack.so

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P .. https://raw.githubusercontent.com/Tomiwa-Akinola/alx-low_level_programming/master/0x18-dynamic_libraries/libhack.so
-export LD_PRELOAD="$PWD/../libhack.so"
+wget -P .. https://raw.githubusercontent.com/Tomiwa-Akinola/alx-low_level_programming/master/0x18-dynamic_libraries/libtest.so
+export LD_PRELOAD="$PWD/../libtest.so"
