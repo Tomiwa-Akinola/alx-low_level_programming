0b97212d9e9791951fdcdadd726219fd011d1e01
tomiwa <codertomiwa@gmail.com>
2024-01-15 05:29:27 -0800

    100-operations.so

---
+++ 0x18-dynamic_libraries/mod.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+
+/**
+ * mod - modulo two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: modulus
+ */
+
+int mod (int a, int b)
+{
+	return (a % b);
+}
---
+++ 0x18-dynamic_libraries/sub.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+
+/**
+ * sub - subtracts two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: subtraction
+ */
+
+int sub (int a, int b)
+{
+	return (a - b);
+}
